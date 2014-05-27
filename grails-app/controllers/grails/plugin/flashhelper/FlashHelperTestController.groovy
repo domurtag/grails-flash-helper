70409f0cbf74b7512a6881c0d14a3232e2ca27a7
domurtag <domurtag@yahoo.co.uk>
2014-05-27 14:01:23 +0100

    refactored integration tests such that FlashHelperTestController can be removed

--- grails-app/controllers/grails/plugin/flashhelper/FlashHelperTestController.groovy
+++
@@ -1,9 +0,0 @@
-package grails.plugin.flashhelper
-
-/**
- * The sole purpose of this controller is to provide a subject for the unit test. It will not be included in the
- * packaged plugin
- * 
- */
-public class FlashHelperTestController {
-}
--- test/integration/grails/plugin/flashhelper/FlashHelperTests.groovy
+++ test/integration/grails/plugin/flashhelper/FlashHelperTests.groovy
@@ -1,18 +1,18 @@
 package grails.plugin.flashhelper
 
-import grails.test.ControllerUnitTestCase
 import org.codehaus.groovy.grails.commons.GrailsApplication
+import org.codehaus.groovy.grails.web.servlet.FlashScope
+import org.codehaus.groovy.grails.web.servlet.GrailsFlashScope
+import org.springframework.context.MessageSource
 import org.springframework.context.NoSuchMessageException
 import org.springframework.context.support.DefaultMessageSourceResolvable
 
-
-/**
- * Tests for <code>FlashHelper</code>. I'm using <code>ControllerUnitTestCase</code> only as a means of getting
- * a reference to a controller, which I'll need to instantiate the <code>FlashHelper</code>
- */
-public class FlashHelperTestControllerTests extends ControllerUnitTestCase {
+public class FlashHelperTests extends GroovyTestCase {
 
     GrailsApplication grailsApplication
+    MessageSource messageSource
+
+    private FlashScope flash
 
     /**
      * Creates a single message or a list of messages
@@ -22,40 +22,42 @@ public class FlashHelperTestControllerTests extends ControllerUnitTestCase {
     }
 
     private FlashHelper getFlashHelper() {
-        controller.flashHelper
+
+        // the FlashHelper's controller constructor arg doesn't have to be an actual controller,
+        // anything that has a .flash property which returns a flash scope will suffice
+        flash = new GrailsFlashScope()
+        def mockController = new Object()
+
+        mockController.metaClass.getFlash = {
+            flash
+        }
+
+        new FlashHelper(mockController, messageSource, grailsApplication.config.flashHelper)
     }
 
-    /**
-     * flashHelper.info "message number 1"
-     * flashHelper.info ["message number 2", "message number 3"]
-     */
     void testLiteralMessages() {
 
         def fh = getFlashHelper()
 
         // Add a single message
         fh.info "foo"
-        assertEquals(["foo"], controller.flash.info)
+        assertEquals(["foo"], flash.info)
 
         // Add another messages
         fh.info "bar"
 
         // Flash should now hold 2 messages in a List
-        assertEquals(["foo", "bar"], controller.flash.info)
+        assertEquals(["foo", "bar"], flash.info)
     }
 
-    /**
-     * flashHelper.info "key1"
-     * flashHelper.info ["key2", "key3"]
-     */
     void testMsgKeysWithoutArgs() {
 
         def fh = getFlashHelper()
         fh.info 'key1'
-        assertEquals getMessages(), controller.flash.info[0]
+        assertEquals getMessages(), flash.info[0]
 
         fh.info 'key2'
-        assertEquals getMessages(1..2), controller.flash.info
+        assertEquals getMessages(1..2), flash.info
     }
 
     /**
@@ -67,13 +69,13 @@ public class FlashHelperTestControllerTests extends ControllerUnitTestCase {
 
         // test a MessageSourceResolvable without args
         fh.info new DefaultMessageSourceResolvable('key1')
-        assertEquals 'message number 1', controller.flash.info[0]
+        assertEquals 'message number 1', flash.info[0]
         fh.clear()
 
         // test a MessageSourceResolvable with args
         String[] msgCodes = ['1ArgMsg']
         fh.info new DefaultMessageSourceResolvable(msgCodes, ['arg'].toArray())
-        assertEquals 'message number arg', controller.flash.info[0]
+        assertEquals 'message number arg', flash.info[0]
     }
 
     /**
@@ -89,32 +91,32 @@ public class FlashHelperTestControllerTests extends ControllerUnitTestCase {
 
         // This should substitute 'key1' into the message, i.e. 'key1' should not be resolved from the resource bundle
         fh.info '1ArgMsg': resolvableKey
-        assertEquals 'message number key1', controller.flash.info[0]
+        assertEquals 'message number key1', flash.info[0]
         fh.clear()
 
         // Same here because argument resolution is turned off by default
         fh.info '1ArgMsg': resolvableKey, false
-        assertEquals 'message number key1', controller.flash.info[0]
+        assertEquals 'message number key1', flash.info[0]
         fh.clear()
 
         // argument should be resolved
         fh.info '1ArgMsg': resolvableKey, true
-        assertEquals "message number $resolvedKey", controller.flash.info[0]
+        assertEquals "message number $resolvedKey", flash.info[0]
         fh.clear()
 
         // If an attempt to resolve an argument fails, the literal argument should be used
         fh.info '1ArgMsg': unresolvableKey, true
-        assertEquals "message number $unresolvableKey", controller.flash.info[0]
+        assertEquals "message number $unresolvableKey", flash.info[0]
         fh.clear()
 
         // A mixture of arguments that can and cannot be resolved
         fh.info '2ArgMsg': [unresolvableKey, resolvableKey], true
-        assertEquals "message $unresolvableKey $resolvedKey", controller.flash.info[0]
+        assertEquals "message $unresolvableKey $resolvedKey", flash.info[0]
         fh.clear()
         
         // As above, but using the named arg API
         fh.info(msgs: ['2ArgMsg': [unresolvableKey, resolvableKey]], resolveArgs: true)
-        assertEquals "message $unresolvableKey $resolvedKey", controller.flash.info[0]
+        assertEquals "message $unresolvableKey $resolvedKey", flash.info[0]
     }
 
     /**
@@ -130,26 +132,26 @@ public class FlashHelperTestControllerTests extends ControllerUnitTestCase {
         // Test Locale
         def fh = getFlashHelper()
         fh.info 'key1', frenchLocale
-        assertEquals frenchMessages[0], controller.flash.info[0]
+        assertEquals frenchMessages[0], flash.info[0]
 
         fh.info 'key2', frenchLocale
-        assertEquals frenchMessages[0..1], controller.flash.info
+        assertEquals frenchMessages[0..1], flash.info
         fh.clear()
 
         // Test default message is ignored when key is found in resource bundles
         fh.info 'key1', frenchLocale, 'default'
-        assertEquals frenchMessages[0], controller.flash.info[0]
+        assertEquals frenchMessages[0], flash.info[0]
 
         fh.info('key2', frenchLocale, 'default2')
-        assertEquals frenchMessages[0..1], controller.flash.info
+        assertEquals frenchMessages[0..1], flash.info
         fh.clear()
 
         // Test default message is used when key is not found in resource bundles
         fh.info 'badKey', frenchLocale, defaultMsgs[0]
-        assertEquals defaultMsgs[0], controller.flash.info[0]
+        assertEquals defaultMsgs[0], flash.info[0]
 
         fh.info 'badKey2', frenchLocale, defaultMsgs[1]
-        assertEquals(defaultMsgs[0..1], controller.flash.info)
+        assertEquals(defaultMsgs[0..1], flash.info)
     }
 
     /**
@@ -161,11 +163,11 @@ public class FlashHelperTestControllerTests extends ControllerUnitTestCase {
 
         // add a message that takes one arg
         fh.info '1ArgMsg': 1
-        assertEquals getMessages(), controller.flash.info[0]
+        assertEquals getMessages(), flash.info[0]
 
         // add a messages that takes two args
         fh.info '2ArgMsg': ['number', '2']
-        assertEquals getMessages(1..2), controller.flash.info
+        assertEquals getMessages(1..2), flash.info
     }
 
     /**
@@ -181,31 +183,31 @@ public class FlashHelperTestControllerTests extends ControllerUnitTestCase {
         // Test Locale
         def fh = getFlashHelper()
         fh.info '1ArgMsg': 1, frenchLocale
-        assertEquals frenchMessages[0], controller.flash.info[0]
+        assertEquals frenchMessages[0], flash.info[0]
 
         fh.info '2ArgMsg': ['numero', 2], frenchLocale
-        assertEquals frenchMessages[0..1], controller.flash.info
+        assertEquals frenchMessages[0..1], flash.info
         fh.clear()
 
         // Test default message is ignored when key is found in resource bundles
         fh.info '1ArgMsg': 1, frenchLocale, defaultMsgs[0]
-        assertEquals frenchMessages[0], controller.flash.info[0]
+        assertEquals frenchMessages[0], flash.info[0]
 
         fh.info '2ArgMsg': ['numero', 2], frenchLocale, defaultMsgs[1]
-        assertEquals frenchMessages[0..1], controller.flash.info
+        assertEquals frenchMessages[0..1], flash.info
         fh.clear()
 
         // Test default message is used when key is not found in resource bundles
         fh.info 'badKey': 1, frenchLocale, defaultMsgs[0]
-        assertEquals defaultMsgs[0], controller.flash.info[0]
+        assertEquals defaultMsgs[0], flash.info[0]
 
         fh.info 'badKey2': ['numero', 2], frenchLocale, defaultMsgs[1]
-        assertEquals(defaultMsgs[0..1], controller.flash.info)
+        assertEquals(defaultMsgs[0..1], flash.info)
         fh.clear()
 
         // Default messages without a Locale
         fh.info 'badKey2': ['numero', 2], 'default msg'
-        assertEquals(['default msg'], controller.flash.info)
+        assertEquals(['default msg'], flash.info)
     }
 
     /**
@@ -217,22 +219,22 @@ public class FlashHelperTestControllerTests extends ControllerUnitTestCase {
         
         // a literal message
         fh.info(msgs: getMessages())
-        assertEquals getMessages(), controller.flash.info[0]
+        assertEquals getMessages(), flash.info[0]
         fh.clear()
         
         // A message key with an arg and a Locale
         fh.info(msgs: ['1ArgMsg': 1], locale: Locale.FRENCH)
-        assertEquals "message numero 1", controller.flash.info[0]
+        assertEquals "message numero 1", flash.info[0]
         fh.clear()
         
         // Use default msg because key doesn't exist
         fh.info(msgs: ['badKey': 1], default: "default message")
-        assertEquals "default message", controller.flash.info[0]
+        assertEquals "default message", flash.info[0]
         fh.clear()
         
         // Multiple messages using method chainging. Each message has args
         fh.info(msgs: ['2ArgMsg': ['arg', 2]]).info(msgs: ['other2ArgMsg': ['arg', 3]], locale: Locale.FRENCH)
-        assertEquals(["message arg 2", "message arg 3"], controller.flash.info)
+        assertEquals(["message arg 2", "message arg 3"], flash.info)
     }
     
     /**
@@ -259,18 +261,18 @@ public class FlashHelperTestControllerTests extends ControllerUnitTestCase {
         def fh = getFlashHelper()
         fh.info(["msg1", "msg2"])
         fh.clear()
-        assertEquals 0, controller.flash.size()
+        assertEquals 0, flash.size()
     }
 
     void testMethodChaining() {
         def fh = getFlashHelper().info("infoMsg").warn("warnMsg")
 
-        assertEquals(['infoMsg'], controller.flash.info)
-        assertEquals(['warnMsg'], controller.flash.warn)
+        assertEquals(['infoMsg'], flash.info)
+        assertEquals(['warnMsg'], flash.warn)
 
         // remove all messages, then add one
         fh.clear().error "errorMsg"
-        assertEquals 1, controller.flash.size()
+        assertEquals 1, flash.size()
     }
 
     /**
